5ce9a69a0214d6afd228824e128dfbb45c82800f
erickgamez15 <erikcgamezsanchez@gmail.com>
2021-11-12 13:27:11 -0600

    Reestructura de Carpetas

---
+++ crudPC.sql
@@ -0,0 +1,35 @@
+create database crudPC
+go
+use crudPC
+go
+create table cliente(
+	idCliente int primary key identity,
+	nombreCompleto varchar(100),
+	correo varchar(50),
+	rfc varchar(20),
+	fechaCreacion datetime default getdate()
+)
+go
+
+create table producto(
+	idProducto int primary key identity,
+	codigo varchar(50),
+	nombreProducto varchar(100),
+	descripcion varchar(100),
+	stock int not null default 0,
+	precioCompra decimal,
+	precioVenta decimal,
+	fechaCreacion datetime default getdate()
+)
+go
+
+create table venta(
+	idVenta int primary key identity,
+	idProducto int references producto(idProducto),
+	idCliente int references cliente(idCliente),
+	nombreCliente varchar(100),
+	montoPago decimal(10,4),
+	montoCambio decimal(10,4),
+	montoTotal decimal(10,4),
+	fechaRegistro datetime default getdate()
+)
